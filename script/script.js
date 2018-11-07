d8772a529b1da0822291a81ba9cd5ae9cd9cb64c
KovalovIE <kovalevie1@gmail.com>
2018-11-07 16:27:16 +0200

    add data in WebSQL

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ var addNewTaskSection = document.getElementById('addNewTaskSection');
 var textNewTask = document.getElementById('textNewTask');
 var deleteTaskButton = document.querySelectorAll('.close');
 var inputs = document.getElementsByTagName('input');
+var dbIdCounter = 0;
 
 
 addTasksBtn.addEventListener('click', function() {
@@ -16,6 +17,7 @@ removeWindowMinimize.addEventListener('click', function() {
 
 textNewTask.addEventListener('keydown', addNewTaskDiv);
 function addNewTaskDiv(event) {
+    var todoInputText = textNewTask.value;
     if (event.keyCode === 13 && textNewTask.value !== '') {
 
         var div = document.createElement('div');
@@ -44,15 +46,15 @@ function addNewTaskDiv(event) {
         var inputs = document.getElementsByTagName('input');
         for( var i = 0; i < inputs.length; i++) {
             if(inputs[i].type === 'checkbox') {
-                inputs[i].addEventListener('click', function() {
-                    var parentDiv = this.parentElement;
-                    parentDiv.classList.toggle('opacity-div-task');
-                    var siblingElement = this.nextElementSibling;
-                    siblingElement.classList.toggle('text-done-task');
-
-                });
+                inputs[i].addEventListener('click', doneTask);
             }
         }
+        console.log(dbIdCounter);
+        db.transaction(function (tx) { // добавили данные в БД
+            tx.executeSql('INSERT INTO TODO (id, task) VALUES (?, ?)', [dbIdCounter, todoInputText]);
+        });
+        dbIdCounter++;
+        console.log(dbIdCounter);
     }
 }
 
@@ -64,17 +66,38 @@ function removeDiv() {
     addNewTaskSection.removeChild(removeDiv);
 }
 
+
 for( var i = 0; i < inputs.length; i++) {
     if(inputs[i].type === 'checkbox') {
-        inputs[i].addEventListener('click', function() {
-            console.log('hello');
-            var parentDiv = this.parentElement;
-            console.log(parentDiv)
-            parentDiv.classList.toggle('opacity-div-task');
-            var siblingElement = this.nextElementSibling;
-            siblingElement.classList.toggle('text-done-task');
-
-        });
+        inputs[i].addEventListener('click', doneTask);
     }
 }
+function doneTask() {
+    console.log('hello');
+    var parentDiv = this.parentElement;
+    parentDiv.classList.toggle('opacity-div-task');
+    var siblingElement = this.nextElementSibling;
+    siblingElement.classList.toggle('text-done-task');
+}
 
+
+var db = openDatabase('mydb', '1.0', 'Todo DB', 2 * 1024 * 1024);
+
+db.transaction(function (tx) { // создали БД
+    tx.executeSql('CREATE TABLE IF NOT EXISTS TODO (id, task)');
+});
+
+db.transaction(function (tx) { // получить элемент из БД
+    tx.executeSql('SELECT * FROM TODO', [], function (tx, data) {
+        var changeText = document.getElementsByTagName('p');
+        //console.log(data.rows[0].task, data.rows[1].task);
+        for (var i = 0; i < data.rows.length; i++) {
+            changeText[i].innerText = data.rows[i].task;
+            console.log(data.rows[i].id, data.rows[i].task)
+        }
+    });
+});
+
+db.transaction(function (tx) { // удалить указанный id
+    tx.executeSql("DELETE FROM TODO WHERE ID=?",[3]);
+});
